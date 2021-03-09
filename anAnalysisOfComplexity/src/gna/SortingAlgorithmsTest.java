4fdd8bb67bfc6a283c907a50e52774bc833f9f55
nickatillinois <nisse.degelin@hotmail.com>
2021-03-09 20:50:51 +0100

    d

--- anAnalysisOfComplexity/src/gna/SortingAlgorithmsTest.java
+++ anAnalysisOfComplexity/src/gna/SortingAlgorithmsTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 /**
  * Tests that test whether the implementations of the sorting algorithms do sort.
  */
+// Pak een stuk of 5 timings van lijsten tussen 0 en 100. Vergelijk tijden met tijdscomplexiteit
 public class SortingAlgorithmsTest {
 
 	@Test
